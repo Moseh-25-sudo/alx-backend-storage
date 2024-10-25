aadc77d2c950e9d3136774c3c406870c4ca6f9f4
Moseh-25-sudo <mosesnganga186@gmail.com>
2024-10-25 16:59:09 -0700

    Updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,42 +1,87 @@
 #!/usr/bin/env python3
-""" Module for Redis db """
+"""doc doc module"""
+
 import redis
-from uuid import uuid4
+import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
 
 
-UnionOfTypes = Union[str, bytes, int, float]
+def count_calls(method: Callable) -> Callable:
+    """doc doc class"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc doc class"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """doc doc class"""
+    inkey = method.__qualname__ + ":inputs"
+    outkey = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc doc class"""
+        self._redis.rpush(inkey, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(outkey, str(res))
+        return res
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """doc doc class"""
+    input_key = "{}:inputs".format(method.__qualname__)
+    output_key = "{}:outputs".format(method.__qualname__)
+
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
+    for inp, out in zip(inputs, outputs):
+        print(
+            "{}(*{}) -> {}".format(
+                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
+            )
+        )
 
 
 class Cache:
-    """ Class for methods that operate a caching system """
+    """doc doc class"""
 
     def __init__(self):
-        """ Instance of the Redis db """
+        """doc doc method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: UnionOfTypes) -> str:
-        """
-        Method takes a data argument and returns a string
-        """
-        self._key = str(uuid4())
-        self._redis.set(self._key, data)
-        return self._key
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """doc doc method"""
+        keyx = str(uuid.uuid4())
+        self._redis.set(keyx, data)
+        return keyx
 
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> UnionOfTypes:
-        """
-        Retrieves data stored in redis using a key
-        converts the result/value back to the desired format
-        """
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float]:
+        """doc doc method"""
         value = self._redis.get(key)
-        return fn(value) if fn else value
+        if fn:
+            value = fn(value)
+        return value
 
-    def get_str(self, value: str) -> str:
-        """ get a string """
-        return self.get(self._key, str)
+    def get_str(self, key: str) -> str:
+        """doc doc method"""
+        return self.get(key, fn=str)
 
-    def get_int(self, value: str) -> int:
-        """ get an int """
-        return self.get(self._key, int)
+    def get_int(self, key: str) -> int:
+        """doc doc method"""
+        return self.get(key, fn=int)
